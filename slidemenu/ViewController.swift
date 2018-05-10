d5b2a7f29784bc073a582db445f96c263773f812
Eugene Liu <s104321015@mail1.ncnu.edu.tw>
2018-05-11 01:51:40 +0800

    one segue

--- slidemenu/ViewController.swift
+++ slidemenu/ViewController.swift
@@ -10,21 +10,11 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBOutlet weak var viewConstraint: NSLayoutConstraint!
-    @IBOutlet weak var blurView: UIVisualEffectView!
-    @IBOutlet weak var slideView: UIView!
-
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        navigationController?.navigationBar.prefersLargeTitles = true 
-        
-        blurView.layer.cornerRadius = 15
-        slideView.layer.shadowColor = UIColor.black.cgColor
-        slideView.layer.shadowOpacity = 0.8
-        slideView.layer.shadowOffset = CGSize(width: 5, height: 0)
-        viewConstraint.constant = -175 //hide slideView
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -33,50 +23,20 @@ class ViewController: UIViewController
     {
         return .lightContent
     }
-
-    @IBAction func panPerformed(_ sender: UIPanGestureRecognizer)
+    
+    @IBAction func goGoTable(_ sender: UIButton)
     {
-        if sender.state == .began || sender.state == .changed
-        {
-            let translation = sender.translation(in: self.view).x //will give x and y value but only need x
-            
-            if translation > 0 //swipe right
-            {
-                if viewConstraint.constant < 20
-                {
-                    UIView.animate(withDuration: 0.2, animations: {self.viewConstraint.constant += translation / 10; self.view.layoutIfNeeded()})
-                }
-            }
-            else //swipe left
-            {
-                if viewConstraint.constant > -175
-                {
-                    UIView.animate(withDuration: 0.2, animations: {self.viewConstraint.constant += translation / 10; self.view.layoutIfNeeded()})
-                }
-            }
-        }
-        
-        else if sender.state == .ended
-        {
-            
-            if viewConstraint.constant < -100
-            {
-                UIView.animate(withDuration: 0.2, animations: {self.viewConstraint.constant = -175; self.view.layoutIfNeeded()})
-            }
-            else
-            {
-                UIView.animate(withDuration: 0.2, animations: {self.viewConstraint.constant = 0; self.view.layoutIfNeeded()})
-            }
-        }
+        self.performSegue(withIdentifier: "goTable", sender: sender.tag)
     }
-    
-    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
-        switch segue.identifier
+        
+        let tag = sender as! Int
+
+        switch tag
         {
-            case "segueToBaseball":
+            case 0:
                 let destinationController = segue.destination as! sportsTableViewController
                 destinationController.firstName = ["Derek", "Alex", "Chien-Ming", "David", "Mariano"]
                 destinationController.LastName = ["Jeter", "Rodriguez", "Wang", "Ortiz", "Rivera"]
@@ -84,7 +44,7 @@ class ViewController: UIViewController
                 destinationController.position = ["SS", "3B", "P", "DH", "RP"]
                 destinationController.team = ["NY Yankees", "NY Yankees", "NY Yankees", "Boston Red Sox", "NY Yankees"]
             
-            case "segueToBasketball":
+            case 1:
                 let destinationController = segue.destination as! sportsTableViewController
                 destinationController.firstName = ["Michael", "Magic", "Kobe", "Lebron", "Stephen"]
                 destinationController.LastName = ["Jordan", "Johnson", "Bryant", "James", "Curry"]
@@ -92,8 +52,7 @@ class ViewController: UIViewController
                 destinationController.position = ["Shooting Guard", "Point Guard", "Shooting Guard", "Small Foward", "Point Guard"]
                 destinationController.team = ["Chicago Bulls", "Los Angles Lakers", "Los Angles Lakers", "Cleveland Cavaliers", "Golden State Warriors"]
             
-            
-            case "segueToSoccer":
+            case 2:
                 let destinationController = segue.destination as! sportsTableViewController
                 destinationController.firstName = ["Zlatan", "Lionel", "Cristiano", "Neymar", "Gareth"]
                 destinationController.LastName = ["Ibrahimovic", "Messi", "Ronaldo", "", "Bale"]
@@ -101,7 +60,7 @@ class ViewController: UIViewController
                 destinationController.position = ["Forward", "Forward",  "Forward", "Forward","Midfielder"]
                 destinationController.team = ["Los Angles Galaxy", "FC Barcelona", "Real Madrid", "Paris Saint Germain", "Real Madrid"]
             
-            case "segueToFootball":
+            case 3:
                 let destinationController = segue.destination as! sportsTableViewController
                 destinationController.firstName = ["Tom", "Odell", "Joe", "Jerry", "Adrian"]
                 destinationController.LastName = ["Brady", "Beckham Jr", "Montana", "Rice", "Peterson"]
@@ -109,7 +68,7 @@ class ViewController: UIViewController
                 destinationController.position = ["Quarterback", "Wide Receiver",  "Quarterback", "Wide Receiver","Running Back"]
                 destinationController.team = ["New England Patriots", "New York Giants", "San Francisco 49ers", "San Francisco 49ers", "Minnesota Vikings"]
             
-            case "segueToHockey":
+            case 4:
                 let destinationController = segue.destination as! sportsTableViewController
                 destinationController.firstName = ["Wayne", "Patrick", "Jonathan", "Sidney", "Patrick"]
                 destinationController.LastName = ["Gretzky", "Kane", "Toews", "Crosby", "Roy"]
@@ -121,7 +80,7 @@ class ViewController: UIViewController
                 break
         }
     }
-    
+  
     /* override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
